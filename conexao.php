c11e920bd123d4e7d8974622ae0e8b54526dee4b
brennotortoza <brenno.tortoza@gmail.com>
2026-04-16 13:52:07 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,12 +1,10 @@
 <?php
-// Pega os dados reais do painel do Railway
-$host = "mysql.railway.internal"
-$user = "root"
+$host = "mysql.railway.internal";
+$user = "root";
 $pass = "uYbOgZGxOSIZVefbqwOsXZIKDasRtjrd";
-$db   = "railway"
+$db   = "railway";
 $port = "3306";
 
-// Faz a conexão usando as variáveis (SEM aspas nos nomes das variáveis)
 $ocon = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$ocon) {
